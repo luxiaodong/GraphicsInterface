f5080f5f6da84089b8521ab16375307344dc7acd
Shadow <luxiaodong@me.com>
2022-05-20 09:02:54 +0800

    add logic devices

--- Vulkan/Vulkan_Tutorial/Vulkan/main.cpp
+++ Vulkan/Vulkan_Tutorial/Vulkan/main.cpp
@@ -9,7 +9,8 @@
 //#include "HelloTriangle/02_instance.cpp"
 //#include "HelloTriangle/03_validation_layers.cpp"
 //#include "HelloTriangle/03_validation_layers_message_callback.cpp"
-#include "HelloTriangle/04_physical_devices.cpp"
+//#include "HelloTriangle/04_physical_devices.cpp"
+#include "HelloTriangle/04_physical_devices_logic_devices_queue.cpp"
 
 int main(int argc, const char * argv[])
 {
