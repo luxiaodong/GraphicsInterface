eb51d4c60edb5ebaef8c63394982b5b10656f924
Shadow <luxiaodong@me.com>
2022-05-29 23:37:28 +0800

    add swapchain recreation

--- Vulkan/Vulkan_Tutorial/Vulkan/main.cpp
+++ Vulkan/Vulkan_Tutorial/Vulkan/main.cpp
@@ -23,7 +23,8 @@
 //#include "HelloTriangle/14_command_buffers.cpp"
 //#include "HelloTriangle/15_rendering_presentation.cpp"
 //#include "HelloTriangle/16_frames_in_flight.cpp"
-#include "HelloTriangle/16_hello_triangle.cpp"
+//#include "HelloTriangle/16_hello_triangle.cpp"
+#include "HelloTriangle/17_swap_chain_recreation.cpp"
 
 int main(int argc, const char * argv[])
 {
