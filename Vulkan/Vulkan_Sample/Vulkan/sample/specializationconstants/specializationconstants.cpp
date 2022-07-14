5da15eeb6e692d4b23360cbc0089901ce5f77464
Shadow <luxiaodong@me.com>
2022-07-14 11:07:49 +0800

    支持 SpecializationInfo, 将一个数据直接传送到着色器

--- Vulkan/Vulkan_Sample/Vulkan/sample/specializationconstants/specializationconstants.cpp
+++ Vulkan/Vulkan_Sample/Vulkan/sample/specializationconstants/specializationconstants.cpp
@@ -22,43 +22,49 @@ void SpecializationConstants::init()
 
 void SpecializationConstants::initCamera()
 {
-    m_camera.setPosition(glm::vec3(2.0f, 4.0f, -9.0f));
-    m_camera.setRotation(glm::vec3(-25.0f, 15.0f, 0.0f));
+    m_camera.setTranslation(glm::vec3(0.0f, 0.0f, -2.0f));
+    m_camera.setRotation(glm::vec3(-40.0f, -90.0f, 0.0f));
     m_camera.setRotationSpeed(0.5f);
-    m_camera.setPerspective(60.0f, (float)(m_width/3.0f) / (float)m_height, 0.1f, 256.0f);
+    m_camera.setPerspective(60.0f, (float)(m_width/3.0f) / (float)m_height, 0.1f, 512.0f);
 }
 
-void SpecializationConstants::setEnabledFeatures()
-{
-    if( m_deviceFeatures.fillModeNonSolid )
-    {
-        m_deviceEnabledFeatures.fillModeNonSolid = VK_TRUE;
-        if( m_deviceFeatures.wideLines )
-        {
-            m_deviceEnabledFeatures.wideLines = VK_TRUE;
-        }
-    }
-}
+//void SpecializationConstants::setEnabledFeatures()
+//{
+//    if( m_deviceFeatures.fillModeNonSolid )
+//    {
+//        m_deviceEnabledFeatures.fillModeNonSolid = VK_TRUE;
+//        if( m_deviceFeatures.wideLines )
+//        {
+//            m_deviceEnabledFeatures.wideLines = VK_TRUE;
+//        }
+//    }
+//}
 
 void SpecializationConstants::clear()
 {
     vkDestroyPipeline(m_device, m_phong, nullptr);
     vkDestroyPipeline(m_device, m_toon, nullptr);
-    vkDestroyPipeline(m_device, m_wireframe, nullptr);
+    vkDestroyPipeline(m_device, m_textured, nullptr);
     
     vkFreeMemory(m_device, m_uniformMemory, nullptr);
     vkDestroyBuffer(m_device, m_uniformBuffer, nullptr);
+    
+    if(m_colorMap)
+    {
+        m_colorMap->clear();
+        delete m_colorMap;
+    }
+    
     m_gltfLoader.clear();
-
     Application::clear();
 }
 
 void SpecializationConstants::prepareVertex()
 {
-//    m_gltfLoader.loadFromFile(Tools::getModelPath() + "triangle.gltf", m_graphicsQueue);
     m_gltfLoader.loadFromFile(Tools::getModelPath() + "color_teapot_spheres.gltf", m_graphicsQueue);
     m_gltfLoader.createVertexAndIndexBuffer();
-    m_gltfLoader.setVertexBindingAndAttributeDescription({VertexComponent::Position, VertexComponent::Normal, VertexComponent::Color});
+    m_gltfLoader.setVertexBindingAndAttributeDescription({VertexComponent::Position, VertexComponent::Normal, VertexComponent::UV, VertexComponent::Color});
+    m_colorMap = Texture::loadTextrue2D(Tools::getTexturePath() +  "metalplate_nomips_rgba.ktx", m_graphicsQueue, VK_FORMAT_R8G8B8A8_UNORM);
 }
 
 void SpecializationConstants::prepareUniform()
@@ -68,40 +74,81 @@ void SpecializationConstants::prepareUniform()
     Tools::createBufferAndMemoryThenBind(uniformSize, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
                                          VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
                                          m_uniformBuffer, m_uniformMemory);
+    
+    Uniform mvp = {};
+    mvp.viewMatrix = m_camera.m_viewMat;
+    mvp.projectionMatrix = m_camera.m_projMat;
+    mvp.lightPos = glm::vec4(0.0f, -2.0f, 1.0f, 0.0f);
+    Tools::mapMemory(m_uniformMemory, sizeof(Uniform), &mvp);
 }
 
 void SpecializationConstants::prepareDescriptorSetLayoutAndPipelineLayout()
 {
-    VkDescriptorSetLayoutBinding binding = Tools::getDescriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT, 0);
-    createDescriptorSetLayout(&binding, 1);
+    std::array<VkDescriptorSetLayoutBinding, 2> bindings;
+    bindings[0] = Tools::getDescriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT, 0);
+    bindings[1] = Tools::getDescriptorSetLayoutBinding(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT, 1);
+    
+    createDescriptorSetLayout(bindings.data(), static_cast<uint32_t>(bindings.size()));
     createPipelineLayout();
 }
 
 void SpecializationConstants::prepareDescriptorSetAndWrite()
 {
-    VkDescriptorPoolSize poolSize = {};
-    poolSize.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-    poolSize.descriptorCount = 1;
-    createDescriptorPool(&poolSize, 1, 1);
+    std::array<VkDescriptorPoolSize, 2> poolSizes;
+    poolSizes[0].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    poolSizes[0].descriptorCount = 1;
+    poolSizes[1].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    poolSizes[1].descriptorCount = 1;
+    createDescriptorPool(poolSizes.data(), static_cast<uint32_t>(poolSizes.size()), 1);
     createDescriptorSet(m_descriptorSet);
 
     VkDescriptorBufferInfo bufferInfo = {};
     bufferInfo.offset = 0;
-    bufferInfo.range = sizeof(Uniform);
+    bufferInfo.range = VK_WHOLE_SIZE;
     bufferInfo.buffer = m_uniformBuffer;
+    
+    VkDescriptorImageInfo imageInfo = {};
+    imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+    imageInfo.imageView = m_colorMap->m_imageView;
+    imageInfo.sampler = m_colorMap->m_sampler;
 
-    VkWriteDescriptorSet write = Tools::getWriteDescriptorSet(m_descriptorSet, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 0, &bufferInfo);
-    vkUpdateDescriptorSets(m_device, 1, &write, 0, nullptr);
+    std::array<VkWriteDescriptorSet, 2> writes = {};
+    writes[0] = Tools::getWriteDescriptorSet(m_descriptorSet, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 0, &bufferInfo);
+    writes[1] = Tools::getWriteDescriptorSet(m_descriptorSet, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, &imageInfo);
+    vkUpdateDescriptorSets(m_device, static_cast<uint32_t>(writes.size()), writes.data(), 0, nullptr);
+}
+
+void SpecializationConstants::prepareSpecializationInfo()
+{
+    // Each shader constant of a shader stage corresponds to one map entry
+    // Shader bindings based on specialization constants are marked by the new "constant_id" layout qualifier:
+    //    layout (constant_id = 0) const int LIGHTING_MODEL = 0;
+    //    layout (constant_id = 1) const float PARAM_TOON_DESATURATION = 0.0f;
+
+    // Map entry for the lighting model to be used by the fragment shader
+    m_specializationMapEntries[0].constantID = 0;
+    m_specializationMapEntries[0].size = sizeof(m_specializationData.lightingModel);
+    m_specializationMapEntries[0].offset = 0;
+
+    // Map entry for the toon shader parameter
+    m_specializationMapEntries[1].constantID = 1;
+    m_specializationMapEntries[1].size = sizeof(m_specializationData.toonDesaturationFactor);
+    m_specializationMapEntries[1].offset = offsetof(SpecializationData, toonDesaturationFactor);
+
+    // Prepare specialization info block for the shader stage
+    m_specializationInfo.dataSize = sizeof(SpecializationData);
+    m_specializationInfo.mapEntryCount = static_cast<uint32_t>(m_specializationMapEntries.size());
+    m_specializationInfo.pMapEntries = m_specializationMapEntries.data();
+    m_specializationInfo.pData = &m_specializationData;
 }
 
 void SpecializationConstants::createGraphicsPipeline()
 {
-//    VkPipelineVertexInputStateCreateInfo vertexInput = {};
-//    vertexInput.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-//    vertexInput.vertexBindingDescriptionCount = static_cast<uint32_t>(m_vertexInputBindDes.size());
-//    vertexInput.pVertexBindingDescriptions = m_vertexInputBindDes.data();
-//    vertexInput.vertexAttributeDescriptionCount = static_cast<uint32_t>(m_vertexInputAttrDes.size());
-//    vertexInput.pVertexAttributeDescriptions = m_vertexInputAttrDes.data();
+    VkShaderModule vertModule = Tools::createShaderModule( Tools::getShaderPath() + "specializationconstants/uber.vert.spv");
+    VkShaderModule fragModule = Tools::createShaderModule( Tools::getShaderPath() + "specializationconstants/uber.frag.spv");
+    
+    VkPipelineShaderStageCreateInfo vertShader = Tools::getPipelineShaderStageCreateInfo(vertModule, VK_SHADER_STAGE_VERTEX_BIT);
+    VkPipelineShaderStageCreateInfo fragShader = Tools::getPipelineShaderStageCreateInfo(fragModule, VK_SHADER_STAGE_FRAGMENT_BIT);
 
     VkPipelineInputAssemblyStateCreateInfo inputAssembly = Tools::getPipelineInputAssemblyStateCreateInfo(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST, VK_FALSE);
 
@@ -115,7 +162,8 @@ void SpecializationConstants::createGraphicsPipeline()
 
     std::vector<VkDynamicState> dynamicStates = {
         VK_DYNAMIC_STATE_VIEWPORT,
-        VK_DYNAMIC_STATE_SCISSOR
+        VK_DYNAMIC_STATE_SCISSOR,
+        VK_DYNAMIC_STATE_LINE_WIDTH,
     };
 
     VkPipelineDynamicStateCreateInfo dynamic = Tools::getPipelineDynamicStateCreateInfo(dynamicStates);
@@ -126,12 +174,10 @@ void SpecializationConstants::createGraphicsPipeline()
     VkPipelineColorBlendAttachmentState colorBlendAttachment = Tools::getPipelineColorBlendAttachmentState(VK_FALSE, VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT);
     VkPipelineColorBlendStateCreateInfo colorBlend = Tools::getPipelineColorBlendStateCreateInfo(1, &colorBlendAttachment);
 
-    std::array<VkPipelineShaderStageCreateInfo, 2> shaderStages;
-    VkGraphicsPipelineCreateInfo createInfo = Tools::getGraphicsPipelineCreateInfo(m_pipelineLayout, m_renderPass);
-    createInfo.stageCount = static_cast<uint32_t>(shaderStages.size());
-    createInfo.pStages = shaderStages.data();
-
-//    createInfo.pVertexInputState = vkglTF::Vertex::getPipelineVertexInputState({vkglTF::VertexComponent::Position, vkglTF::VertexComponent::Normal, vkglTF::VertexComponent::Color});
+    std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
+    shaderStages.push_back(vertShader);
+    shaderStages.push_back(fragShader);
+    VkGraphicsPipelineCreateInfo createInfo = Tools::getGraphicsPipelineCreateInfo(shaderStages, m_pipelineLayout, m_renderPass);
 
     createInfo.pVertexInputState = m_gltfLoader.getPipelineVertexInputState();
     createInfo.pInputAssemblyState = &inputAssembly;
@@ -143,69 +189,36 @@ void SpecializationConstants::createGraphicsPipeline()
     createInfo.pColorBlendState = &colorBlend;
     createInfo.pDynamicState = &dynamic;
     createInfo.subpass = 0;
+    
+    prepareSpecializationInfo();
+    // Specialization info is assigned is part of the shader stage (modul) and must be set after creating the module and before creating the pipeline
+    fragShader.pSpecializationInfo = &m_specializationInfo;
 
-    createInfo.flags = VK_PIPELINE_CREATE_ALLOW_DERIVATIVES_BIT;
-
-    // Phong shading pipeline
-    VkShaderModule vertModule = Tools::createShaderModule( Tools::getShaderPath() + "pipelines/phong.vert.spv");
-    VkShaderModule fragModule = Tools::createShaderModule( Tools::getShaderPath() + "pipelines/phong.frag.spv");
-    shaderStages[0] = Tools::getPipelineShaderStageCreateInfo(vertModule, VK_SHADER_STAGE_VERTEX_BIT);
-    shaderStages[1] = Tools::getPipelineShaderStageCreateInfo(fragModule, VK_SHADER_STAGE_FRAGMENT_BIT);
-
+    m_specializationData.lightingModel = 0;
     if( vkCreateGraphicsPipelines(m_device, m_pipelineCache, 1, &createInfo, nullptr, &m_phong) != VK_SUCCESS )
     {
         throw std::runtime_error("failed to create graphics pipeline!");
     }
-
-    vkDestroyShaderModule(m_device, vertModule, nullptr);
-    vkDestroyShaderModule(m_device, fragModule, nullptr);
-
-    // All pipelines created after the base pipeline will be derivatives
-    createInfo.flags = VK_PIPELINE_CREATE_DERIVATIVE_BIT;
-    // Base pipeline will be our first created pipeline
-    createInfo.basePipelineHandle = m_phong;
-    // It's only allowed to either use a handle or index for the base pipeline
-    // As we use the handle, we must set the index to -1 (see section 9.5 of the specification)
-    createInfo.basePipelineIndex = -1;
-
-
-    // Toon shading pipeline
-    vertModule = Tools::createShaderModule( Tools::getShaderPath() + "pipelines/toon.vert.spv");
-    fragModule = Tools::createShaderModule( Tools::getShaderPath() + "pipelines/toon.frag.spv");
-    shaderStages[0] = Tools::getPipelineShaderStageCreateInfo(vertModule, VK_SHADER_STAGE_VERTEX_BIT);
-    shaderStages[1] = Tools::getPipelineShaderStageCreateInfo(fragModule, VK_SHADER_STAGE_FRAGMENT_BIT);
+    
+    m_specializationData.lightingModel = 1;
     if( vkCreateGraphicsPipelines(m_device, m_pipelineCache, 1, &createInfo, nullptr, &m_toon) != VK_SUCCESS )
     {
         throw std::runtime_error("failed to create graphics pipeline!");
     }
+    
+    m_specializationData.lightingModel = 2;
+    if( vkCreateGraphicsPipelines(m_device, m_pipelineCache, 1, &createInfo, nullptr, &m_textured) != VK_SUCCESS )
+    {
+        throw std::runtime_error("failed to create graphics pipeline!");
+    }
+
     vkDestroyShaderModule(m_device, vertModule, nullptr);
     vkDestroyShaderModule(m_device, fragModule, nullptr);
-
-    //wireframe shading pipeline
-    if(m_deviceFeatures.fillModeNonSolid)
-    {
-        rasterization.polygonMode = VK_POLYGON_MODE_LINE;
-        vertModule = Tools::createShaderModule( Tools::getShaderPath() + "pipelines/wireframe.vert.spv");
-        fragModule = Tools::createShaderModule( Tools::getShaderPath() + "pipelines/wireframe.frag.spv");
-        shaderStages[0] = Tools::getPipelineShaderStageCreateInfo(vertModule, VK_SHADER_STAGE_VERTEX_BIT);
-        shaderStages[1] = Tools::getPipelineShaderStageCreateInfo(fragModule, VK_SHADER_STAGE_FRAGMENT_BIT);
-        if( vkCreateGraphicsPipelines(m_device, m_pipelineCache, 1, &createInfo, nullptr, &m_wireframe) != VK_SUCCESS )
-        {
-            throw std::runtime_error("failed to create graphics pipeline!");
-        }
-        vkDestroyShaderModule(m_device, vertModule, nullptr);
-        vkDestroyShaderModule(m_device, fragModule, nullptr);
-    }
 }
 
 void SpecializationConstants::updateRenderData()
 {
-    Uniform mvp = {};
-    mvp.modelMatrix = m_camera.m_viewMat;
-    mvp.viewMatrix = glm::mat4(1.0f);
-    mvp.projectionMatrix = m_camera.m_projMat;
-    mvp.lightPos = glm::vec4(0.0f, 2.0f, 1.0f, 0.0f);
-    Tools::mapMemory(m_uniformMemory, sizeof(Uniform), &mvp);
+    
 }
 
 void SpecializationConstants::recordRenderCommand(const VkCommandBuffer commandBuffer)
@@ -229,15 +242,12 @@ void SpecializationConstants::recordRenderCommand(const VkCommandBuffer commandB
     // toon
     viewport.x = (float)m_swapchainExtent.width/3.0;
     vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
-    if( m_deviceFeatures.wideLines ) vkCmdSetLineWidth(commandBuffer, 2.0f);
     vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_toon);
     m_gltfLoader.draw(commandBuffer);
 
-    if(m_deviceFeatures.fillModeNonSolid)
-    {
-        viewport.x += (float)m_swapchainExtent.width/3.0;
-        vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
-        vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_wireframe);
-        m_gltfLoader.draw(commandBuffer);
-    }
+    //textured
+    viewport.x += (float)m_swapchainExtent.width/3.0;
+    vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+    vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_textured);
+    m_gltfLoader.draw(commandBuffer);
 }
--- Vulkan/Vulkan_Sample/Vulkan/sample/specializationconstants/specializationconstants.h
+++ Vulkan/Vulkan_Sample/Vulkan/sample/specializationconstants/specializationconstants.h
@@ -10,9 +10,16 @@ class SpecializationConstants : public Application
 public:
     struct Uniform {
         glm::mat4 projectionMatrix;
-        glm::mat4 modelMatrix;
+//        glm::mat4 modelMatrix;
         glm::mat4 viewMatrix;
         glm::vec4 lightPos;
+    };
+    
+    struct SpecializationData {
+        // Sets the lighting model used in the fragment "uber" shader
+        uint32_t lightingModel;
+        // Parameter for the toon shading part of the fragment shader
+        float toonDesaturationFactor = 0.5f;
     };
 
     SpecializationConstants(std::string title);
@@ -20,7 +27,7 @@ public:
 
     virtual void init();
     virtual void initCamera();
-    virtual void setEnabledFeatures();
+//    virtual void setEnabledFeatures();
     virtual void clear();
 
     virtual void updateRenderData();
@@ -31,16 +38,22 @@ protected:
     void prepareUniform();
     void prepareDescriptorSetLayoutAndPipelineLayout();
     void prepareDescriptorSetAndWrite();
+    void prepareSpecializationInfo();
     void createGraphicsPipeline();
 
 protected:
     VkDescriptorSet m_descriptorSet;
     VkPipeline m_phong;
     VkPipeline m_toon;
-    VkPipeline m_wireframe;
+    VkPipeline m_textured;
 
     VkBuffer m_uniformBuffer;
     VkDeviceMemory m_uniformMemory;
+    
+    SpecializationData m_specializationData = {};
+    std::array<VkSpecializationMapEntry, 2> m_specializationMapEntries;
+    VkSpecializationInfo m_specializationInfo = {};
+    Texture* m_colorMap = nullptr;
 
 private:
     GltfLoader m_gltfLoader;
